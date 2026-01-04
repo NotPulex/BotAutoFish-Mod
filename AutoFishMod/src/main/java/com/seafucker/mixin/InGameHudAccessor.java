d6c4dd9d9601d646a338da6db1a3a99338435684
Pulex <cristigno07@gmail.com>
2026-01-04 19:12:38 +0100

    FILE BASE
    Struttura base della mod, serve soprattutto ad aiutarmi per comunicare con l'IA

---
+++ AutoFishMod/src/main/java/com/seafucker/mixin/InGameHudAccessor.java
@@ -0,0 +1,15 @@
+package com.seafucker.mixin;
+
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(InGameHud.class)
+public interface InGameHudAccessor {
+    @Accessor("title")
+    Text getTitle();
+
+    @Accessor("subtitle")
+    Text getSubtitle();
+}
